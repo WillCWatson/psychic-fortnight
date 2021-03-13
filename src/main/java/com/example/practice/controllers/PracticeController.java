e781c114feef63957eac22c773f134defe3e99d9
William Watson <williamcwatson@protonmail.com>
2021-03-13 14:49:51 -0500

    Returns JSON Now!

--- src/main/java/com/example/practice/controllers/PracticeController.java
+++ src/main/java/com/example/practice/controllers/PracticeController.java
@@ -12,8 +12,16 @@ public class PracticeController {
     }
 
     @RequestMapping("/hello") // Any time someone comes to the root, this code will run
-    public String hello() {
-        return "The Hello Method!";
+    public PracticeClass hello() {
+        PracticeClass student = new PracticeClass();
+
+        student.fName = "Gary";
+        student.lName = "Watson";
+        student.age = 42;
+
+        return student;
+
     }
+
 }
 
